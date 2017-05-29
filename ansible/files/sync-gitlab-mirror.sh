f38ac3d6e986358c429795f8eaa45a13383edc3e
Dmitriy Andreev <me@kemko.ru>
2017-05-29 19:39:06 +0300

    fix envs

--- ansible/files/sync-gitlab-mirror.sh
+++ ansible/files/sync-gitlab-mirror.sh
@@ -4,10 +4,9 @@
 --build-events-webhook-url="${BUILD_EVENTS_WEBHOOK_URL}" \
 --gitlab-instance="${GITLAB_INSTANCE_URL}" \
 --github-repo-owner="${GITHUB_ACCOUNT}" \
---github-repo-name="${REPO_NAME}" \
+--github-repo-path="${REPO_NAME}" \
 --github-private-token="${GITHUB_TOKEN}" \
 --gitlab-repo-owner="${GITLAB_ACCOUNT}" \
---gitlab-repo-name="${REPO_NAME}" \
 --ref="${REF}" \
 --gitlab-token="${GITLAB_TOKEN}" \
 --gitlab-runner-id="${GITLAB_CI_RUNNER_ID}" \
